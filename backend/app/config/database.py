ff64bf78ba9faceb82dbfdc33164dbad3d11f241
swaran <bandariswaran77@gmail.com>
2026-06-25 17:27:51 +0530

    Fixed MongoDB SSL handshake issue using certifi CA bundle

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -1,8 +1,9 @@
 ﻿from pymongo import MongoClient
 import os
+import certifi
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://bandariswaran77_db_user:e9xZY0NovNde9d4W@ai-mock-interview.5woxz9n.mongodb.net/?appName=AI-Mock-Interview")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "ai_mock_interview")
-client = MongoClient(MONGO_URI)
+client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
 db = client[DATABASE_NAME]
 def get_database():
     return db
